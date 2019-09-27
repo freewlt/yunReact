72b516ff93e23c92ab53cd070981a24d19f909a5
wangliting <wangliting@e-autolife.com>
2019-09-27 10:18:32 +0800

    web:react云车令

---
+++ precache-manifest.374feab581522dcca738eb85b25f3eaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68aad9aa084a69d7f0f856b028c56238",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23a23c339f15c1cc1083",
+    "url": "./static/css/2.3a24c9eb.chunk.css"
+  },
+  {
+    "revision": "9ac836fe03ec0adff39d",
+    "url": "./static/css/main.774752b3.chunk.css"
+  },
+  {
+    "revision": "23a23c339f15c1cc1083",
+    "url": "./static/js/2.ef233941.chunk.js"
+  },
+  {
+    "revision": "9ac836fe03ec0adff39d",
+    "url": "./static/js/main.a90e0d98.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
